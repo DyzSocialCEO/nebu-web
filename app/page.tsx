5a6c999f8d4e3d71f8e291a728f7c18128353845
DyzSocialCEO <144451315+DyzSocialCEO@users.noreply.github.com>
2026-09-13 18:52:25 +0200

    Mount NEBU Pulse on public experience

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import NebuExperience from "@/components/NebuExperience";
+import NebuPulse from "@/components/NebuPulse";
 import { getSiteData } from "@/lib/site-data";
 
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const data = await getSiteData();
-  return <NebuExperience initialData={data} />;
+  return (
+    <>
+      <NebuExperience initialData={data} />
+      <NebuPulse />
+    </>
+  );
 }
